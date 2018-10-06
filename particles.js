5c0a28517c44a6922558ecab023b2eb3f28f0348
techakhilc47 <43501564+techakhilc47@users.noreply.github.com>
2018-10-06 22:19:57 +0530

    Update particles.js

--- particles.js
+++ particles.js
@@ -20,7 +20,7 @@ var pJS = function(tag_id, params){
     },
     particles: {
       number: {
-        value: 400,
+        value: 600,
         density: {
           enable: true,
           value_area: 800
